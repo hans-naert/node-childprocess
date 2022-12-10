3f5c052591ace27278dcd6034c6082de5951fe69
Hans Naert <hans.naert@vives.be>
2022-12-10 15:58:18 +0100

    test child process

---
+++ index.js
@@ -0,0 +1,12 @@
+const { exec } = require("child_process");
+exec("echo test", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
